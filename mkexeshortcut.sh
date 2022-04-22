69c4c65206aa3d21c0dff1a51b32cb3638f29c0a
drzetein <drzetein@hotmail.com>
2022-04-21 21:47:20 -0300

    Add comments and improve readability
    Added comments and improved text structure for better readability. Declared and used the variable icon_path earlier. Changed a few console messages.

--- mkexeshortcut.sh
+++ mkexeshortcut.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# Basic error checking. #
+
 if [ ! $(command -v wrestool) ]; then
     echo -e "\e[1m\e[31mERROR: Missing dependency\e[0m \e[3m\e[96micoutils\e[0m\e[1m\e[31m.\e[0m"
     exit 3
@@ -20,8 +23,10 @@ if [[ $1 == "--usage" || $1 == "-u" || $1 == "--help" || $1 == "-h" ]]; then
     exit 0
 fi
 
-input="$1"
 
+# Get the input file and check if it is a valid Windows executable. #
+
+input="$1"
 if ( file --mime-type "$input" | grep -o "application/x-dosexec" >/dev/null ); then
     echo -e "\e[1m\e[1;33mWorking on file:\e[0m \e[3m\e[96m\"$input\"\e[0m\e[1m\e[1;33m.\e[0m"
 else
@@ -29,35 +34,44 @@ else
     exit 6
 fi
 
-input_folder=${input%/*}
 
+# Return the input directory by removing the filename from the string. Check if it has write permissions. #
+
+input_folder=${input%/*}
 if [ ! -w "$input_folder" ]; then
     echo -e "\e[1m\e[31mERROR: Input folder is not writable:\e[0m \e[3m\e[96m\"$input_folder\"\e[0m\e[1m\e[31m.\e[0m"
     exit 7
 fi
 
+
+# Get a clean title for the shortcut by removing the extension and replacing underscores by whitespaces. #
+
 input_filename=${input##*/}
 input_title=$(echo ${input_filename%.exe} | sed -s "s/_/ /g")
 
-# Search for user's desktop #
+
+# Set the output location to the user's desktop directory. #
+
 desktop="$(xdg-user-dir DESKTOP)"
 output="$desktop/$input_title.desktop"
 
+
 # If a second argument was given, interpret it as the output location. #
+
 if [ $# == 2 ]; then
     if [ -d "$2" ]; then
         if [ -w "$2" ]; then
             output="$2/$input_title.desktop"
         else
-            echo -e "\e[1m\e[31mERROR: Folder is not writable:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
+            echo -e "\e[1m\e[31mERROR: Output folder is not writable:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
             exit 8
         fi
     else
-        echo -e "\e[1m\e[31mERROR: Parameter is not a folder:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
+        echo -e "\e[1m\e[31mERROR: Argument is not a folder:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
         exit 9
     fi
 elif [ ! -w $desktop ]; then
-    echo -e "\e[1m\e[31mERROR: Folder is not writable:\e[0m \e[3m\e[96m\"$desktop\"\e[0m\e[1m\e[31m.\e[0m"
+    echo -e "\e[1m\e[31mERROR: Output folder is not writable:\e[0m \e[3m\e[96m\"$desktop\"\e[0m\e[1m\e[31m.\e[0m"
     exit 8
 fi
 
@@ -69,12 +83,14 @@ echo -e "\e[1m\e[32mYou're good to go.\e[0m
 | OUTPUT\e[0m   \e[3m\e[96m$output\e[0m
 \e[1m\e[1;33mExtracting icon resources from executable.\e[0m"
 
-wrestool --type=-14 --extract --output="$input_folder/$input_title.ico" "$input"
+
+# Extract icon from the executable. #
+
+icon_path="$input_folder/$input_title.ico"
+wrestool --type=-14 --extract --output="$icon_path" "$input"
 if [[ $? > 0 ]]; then
     echo -e "\e[1m\e[31mAn error has occurred while extracting resources.\e[0m ($?)"
 fi
-
-icon_path="$input_folder/$input_title.ico"
 
 if ( file --mime-type "$icon_path" | grep -o "image/" >/dev/null ); then
     echo -e "\e[1m\e[32mSelected icon file:\e[0m \e[3m\e[96m\"$icon_path\"\e[0m\e[1m\e[32m.\e[0m"
@@ -82,7 +98,7 @@ else
     echo -e "\e[1m\e[1;33mWarning: File is not an image:\e[0m \e[3m\e[96m\"$icon_path\"\e[0m\e[1m\e[1;33m.\e[0m"
     echo -e "\e[1m\e[1;33mTrying to extract alternative icons.\e[0m"
 
-    wrestool --type=-3 --extract --output="$input_folder/$input_title.ico" "$input"
+    wrestool --type=-3 --extract --output="$icon_path" "$input"
     if [[ $? > 0 ]]; then
         echo -e "\e[1m\e[31mAn error has occurred while extracting resources.\e[0m ($?)"
     fi
@@ -95,7 +111,10 @@ else
 fi
 
 echo -e "\e[1m\e[32mFinished extracting resources.\e[0m"
-    
+
+
+# Create the shortcut file. #
+
 echo "[Desktop Entry]
 Type=Application
 MimeType=application/x-dosexec
@@ -131,6 +150,9 @@ Comment[vi]=Khởi chạy $input_title
 Comment[zh]=启动$input_title
 StartupNotify=true
 Terminal=false" > "$output"
+
+
+# Check for last errors and set executable permissions. #
 
 if [ ! -f "$output" ]; then
     echo -e "\e[1m\e[31mERROR: The file\e[0m \e[3m\e[96m\"$output\"\e[0m \e[1m\e[31mcould not be created.\e[0m"
