067cd38337f1e0ecbebb2377a4bc66346888251f
drzetein <drzetein@hotmail.com>
2022-08-28 18:03:48 -0300

    Better icon extraction procedure
    There was some issues with the icon extraction. Because programs use different resource names, some would have the wrong icon extracted. Before, the code would try to extract both resource types number 3 (icon) and 14 (group_icon), and without specifying any resource name. Now, only the first group_icon will be extracted with its name specified, which decreases or eliminates the chances of unintended behaviour.

--- mkexeshortcut.sh
+++ mkexeshortcut.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-
-# Basic error checking. #
-
 if [ ! $(command -v wrestool) ]; then
     echo -e "\e[1m\e[31mERROR: Missing dependency\e[0m \e[3m\e[96micoutils\e[0m\e[1m\e[31m.\e[0m"
     exit 3
 fi
 
 if [ $(id -u) == 0 ]; then
-    echo -e "\e[1m\e[31mERROR: Refusing to run\e[0m \e[3m\e[96mmkexeshortcut\e[0m\e[1m\e[31m as root.\e[0m"
+    echo -e "\e[1m\e[31mERROR: Refusing to run\e[0m \e[3m\e[96mmkExeShortcut\e[0m\e[1m\e[31m as root.\e[0m"
     exit 4
 fi
 
 if [[ $# < 1 || $# > 2 ]]; then
-    echo -e "\e[1m\e[31mERROR: Incorrect number of arguments.\e[0m\n\e[1mUsage:\e[0m \e[3mmkexeshortcut <input_file> [output_folder]\e[0m\e[1m\e[31m.\e[0m"
+    echo -e "\e[1m\e[31mERROR: Incorrect number of arguments.\e[0m\n\e[1mUsage:\e[0m \e[3mmkexeshortcut <filename> [output]\e[0m\e[1m\e[31m.\e[0m"
     exit 5
 fi
 
-if [[ $1 == "--usage" || $1 == "-u" || $1 == "--help" || $1 == "-h" ]]; then
-    echo -e "\e[1mUsage:\e[0m \e[3mmkexeshortcut <input_file> [output_folder]\e[0m\nBy default, the output folder is set to the user's desktop directory. (\e[3mxdg-user-dir DESKTOP\e[0m)"
-    exit 0
-fi
+input="$(readlink -f "$1")"
 
-
-# Get the input file and check if it is a valid Windows executable. #
-
-input="$1"
 if ( file --mime-type "$input" | grep -o "application/x-dosexec" >/dev/null ); then
     echo -e "\e[1m\e[1;33mWorking on file:\e[0m \e[3m\e[96m\"$input\"\e[0m\e[1m\e[1;33m.\e[0m"
 else
@@ -34,86 +24,71 @@ else
     exit 6
 fi
 
-
-# Return the input directory by removing the filename from the string. Check if it has write permissions. #
-
 input_folder=${input%/*}
+
 if [ ! -w "$input_folder" ]; then
     echo -e "\e[1m\e[31mERROR: Input folder is not writable:\e[0m \e[3m\e[96m\"$input_folder\"\e[0m\e[1m\e[31m.\e[0m"
     exit 7
 fi
 
-
-# Get a clean title for the shortcut by removing the extension and replacing underscores by whitespaces. #
-
 input_filename=${input##*/}
 input_title=$(echo ${input_filename%.exe} | sed -s "s/_/ /g")
 
-
-# Set the output location to the user's desktop directory. #
-
-desktop="$(xdg-user-dir DESKTOP)"
-output="$desktop/$input_title.desktop"
-
+# Search for user's desktop #
+output="$(xdg-user-dir DESKTOP)/$input_title.desktop"
 
 # If a second argument was given, interpret it as the output location. #
-
 if [ $# == 2 ]; then
     if [ -d "$2" ]; then
         if [ -w "$2" ]; then
             output="$2/$input_title.desktop"
         else
-            echo -e "\e[1m\e[31mERROR: Output folder is not writable:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
+            echo -e "\e[1m\e[31mERROR: Folder is not writable:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
             exit 8
         fi
     else
-        echo -e "\e[1m\e[31mERROR: Argument is not a folder:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
+        echo -e "\e[1m\e[31mERROR: Parameter is not a folder:\e[0m \e[3m\e[96m\"$2\"\e[0m\e[1m\e[31m.\e[0m"
         exit 9
     fi
-elif [ ! -w $desktop ]; then
-    echo -e "\e[1m\e[31mERROR: Output folder is not writable:\e[0m \e[3m\e[96m\"$desktop\"\e[0m\e[1m\e[31m.\e[0m"
+elif [ ! -w $output ]; then
+    echo -e "\e[1m\e[31mERROR: Folder is not writable:\e[0m \e[3m\e[96m\"$output\"\e[0m\e[1m\e[31m.\e[0m"
     exit 8
 fi
 
-echo -e "\e[1m\e[32mYou're good to go.\e[0m
-\e[1m\e[95mCMD PARAMS
+echo -e "\e[1m\e[32mYou're good to go.\e[0m"
+
+echo -e "\e[1m\e[95mCMD PARAMS
 | FILENAME\e[0m \e[3m\e[96m$input_filename\e[0m \e[1m\e[95m
 | FOLDER\e[0m   \e[3m\e[96m$input_folder\e[0m \e[1m\e[95m
 | TITLE\e[0m    \e[3m\e[96m$input_title\e[0m \e[1m\e[95m
-| OUTPUT\e[0m   \e[3m\e[96m$output\e[0m
-\e[1m\e[1;33mExtracting icon resources from executable.\e[0m"
+| OUTPUT\e[0m   \e[3m\e[96m$output\e[0m"
+
+echo -e "\e[1m\e[1;33mExtracting icon resources from executable.\e[0m"
 
 
-# Extract icon from the executable. #
+# Extract the first group icon in the executable. #
 
+icon_name=$(wrestool --list --type=-14 "$input" | grep -oP -m1 "(?<=--name=|--name=')\w*")
+icon_name_type="unknown"
 icon_path="$input_folder/$input_title.ico"
-wrestool --type=-14 --extract --output="$icon_path" "$input"
+
+if [[ $icon_name =~ ^[0-9]+$ ]]; then
+    icon_name_type="number"
+elif [[ $icon_name =~ ^[a-zA-Z0-9_]+$ ]]; then
+    icon_name_type="string"
+fi
+
+if [[ $icon_name_type == "number" ]]; then
+    wrestool --extract --type=-14 --name=-$icon_name --output="$icon_path" "$input"
+elif [[ $icon_name_type == "string" ]]; then
+    wrestool --extract --type=-14 --name=+$icon_name --output="$icon_path" "$input"
+fi
+
 if [[ $? > 0 ]]; then
-    echo -e "\e[1m\e[31mAn error has occurred while extracting resources.\e[0m ($?)"
-fi
-
-if ( file --mime-type "$icon_path" | grep -o "image/" >/dev/null ); then
-    echo -e "\e[1m\e[32mSelected icon file:\e[0m \e[3m\e[96m\"$icon_path\"\e[0m\e[1m\e[32m.\e[0m"
+    echo -e "\e[1m\e[31mAn error has occurred while extracting icon.\e[0m ($?)"
 else
-    echo -e "\e[1m\e[1;33mWarning: File is not an image:\e[0m \e[3m\e[96m\"$icon_path\"\e[0m\e[1m\e[1;33m.\e[0m"
-    echo -e "\e[1m\e[1;33mTrying to extract alternative icons.\e[0m"
-
-    wrestool --type=-3 --extract --output="$icon_path" "$input"
-    if [[ $? > 0 ]]; then
-        echo -e "\e[1m\e[31mAn error has occurred while extracting resources.\e[0m ($?)"
-    fi
-
-    if ( file --mime-type "$icon_path" | grep -o "image/" >/dev/null ); then
-        echo -e "\e[1m\e[32mSelected icon file:\e[0m \e[3m\e[96m\"$icon_path\"\e[0m\e[1m\e[32m.\e[0m"
-    else
-        echo -e "\e[1m\e[31mWarning: File is not an image:\e[0m \e[3m\e[96m\"$icon_path\"\e[0m\e[1m\e[31m.\e[0m"
-    fi
+    echo -e "\e[1m\e[32mFinished extracting icon\e[0m"
 fi
-
-echo -e "\e[1m\e[32mFinished extracting resources.\e[0m"
-
-
-# Create the shortcut file. #
 
 echo "[Desktop Entry]
 Type=Application
@@ -151,15 +126,12 @@ Comment[zh]=启动$input_title
 StartupNotify=true
 Terminal=false" > "$output"
 
-
-# Check for last errors and set executable permissions. #
-
 if [ ! -f "$output" ]; then
     echo -e "\e[1m\e[31mERROR: The file\e[0m \e[3m\e[96m\"$output\"\e[0m \e[1m\e[31mcould not be created.\e[0m"
     exit 10
 fi
 
-echo -e "\e[1m\e[1;33mSetting executable permissions.\e[0m"
+echo -e "\e[1m\e[1;33mSetting execute permissions.\e[0m"
 
 chmod +x "$input"
 
